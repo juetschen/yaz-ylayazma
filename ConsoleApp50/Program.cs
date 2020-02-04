10d0f58c7f33e1a9de7fe8b716a0171d78158a10
juetschen <60485447+juetschen@users.noreply.github.com>
2020-02-04 20:01:26 +0300

    project pertama

---
+++ ConsoleApp50/Program.cs
@@ -0,0 +1,97 @@
+﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp49
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi;
+
+            sayi = Sayigir();
+            YazıylaYaz(sayi);
+            Console.ReadLine();
+        }
+        static int Sayigir()
+        {
+            string s;
+            Console.WriteLine("Bir Sayı gir");
+            s = Console.ReadLine();
+
+            int b = int.Parse(s);
+            return b;
+        }
+        static void YazıylaYaz(int sy)
+        {
+            if (sy < 10)
+                birlik(sy);
+            else if (sy < 10000)
+            {
+                yüzlük((sy % 1000) / 100);
+                onluk((sy % 100) / 10);
+                birlik((sy % 10));
+
+            }
+            if (sy == 0) Console.WriteLine("Sıfır");
+        }
+        static void birlik(int b)
+        {
+            switch (b)
+            {
+                case 9: Console.Write("Dokuz"); break;
+                case 8: Console.Write("Sekiz"); break;
+                case 7: Console.Write("Yedi"); break;
+                case 6: Console.Write("Altı"); break;
+                case 5: Console.Write("Beş"); break;
+                case 4: Console.Write("Dört"); break;
+                case 3: Console.Write("Üç"); break;
+                case 2: Console.Write("İki"); break;
+                case 1: Console.Write("Bir"); break;
+                default:
+                    break;
+            }
+        }
+
+        static void yüzlük(int b)
+        {
+            switch (b)
+            {
+                case 9: Console.Write("Dokuzyüz "); break;
+                case 8: Console.Write("Sekizyüz "); break;
+                case 7: Console.Write("Yediyüz "); break;
+                case 6: Console.Write("Altıyüz "); break;
+                case 5: Console.Write("Beşyüz "); break;
+                case 4: Console.Write("Dörtyüz "); break;
+                case 3: Console.Write("Üçyüz "); break;
+                case 2: Console.Write("İkiyüz "); break;
+                case 1: Console.Write("Yüz "); break;
+                default:
+                    break;
+            }
+        }
+        static void onluk(int b)
+        {
+            switch (b)
+            {
+                case 9: Console.Write("Doksan "); break;
+                case 8: Console.Write("Seksen "); break;
+                case 7: Console.Write("Yetmiş "); break;
+                case 6: Console.Write("Altmış "); break;
+                case 5: Console.Write("Elli "); break;
+                case 4: Console.Write("Kırk "); break;
+                case 3: Console.Write("Otuz "); break;
+                case 2: Console.Write("Yirmi "); break;
+                case 1: Console.Write("On "); break;
+                default:
+                    break;
+            }
+        }
+
+
+    }
+}
---
+++ ConsoleApp50/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("ConsoleApp50")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp50")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("ddd91498-8d16-48c4-bffd-d473b95c14f8")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// '*' kullanarak varsayılana ayarlayabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
